beeaed7a32b07bd18b8180deba2a2b5f00d7beb9
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-01-25 17:44:34 +0000

    Update vars.sh

--- includes/vars.sh
+++ includes/vars.sh
@@ -1,7 +1,8 @@
 #Colors for logging
-export GREEN='\033[0;32m'
-export RED='\033[0;31m'
-export NC='\033[0m'
+export RED="\033[0;31m"
+export ORANGE="\033[0;33m"
+export GREEN="\033[0;32m"
+export NC="\033[0m"
 
 #Other vars
 export AXIOM_PATH="$HOME/.axiom"
