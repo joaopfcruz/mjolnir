eb87d896d02942dec76ea47d5131f487ae69800d
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-01-25 12:56:26 +0000

    including logger.sh and vars.sh

--- fleetmgmt/axiomfleet-delete.sh
+++ fleetmgmt/axiomfleet-delete.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m'
+source "$MJOLNIR_PATH/includes/vars.sh"
+source "$MJOLNIR_PATH/includes/logger.sh"
 
-AXIOM_PATH="$HOME/.axiom"
-
-usage() { printf "${RED}Usage: $0 -p <fleet prefix>${NC}\n" 1>&2; exit 0; }
+usage() { log_err "Usage: $0 -p <fleet prefix>" 1>&2; exit 0; }
 
 while getopts ":p:" flags; do
   case "${flags}" in
@@ -29,7 +26,7 @@ prefix=$(echo "${prefix}" | tr -dc '[:alnum:]')
 $AXIOM_PATH/interact/axiom-rm "${prefix}*" -f
 
 if ! [ $? -eq 0 ]; then
-  >&2 printf "${RED}error: An error occurred while deleting fleet.${NC}\n"; exit 1;
+  >&2 log_err "An error occurred while deleting fleet."; exit 1;
 else
-  printf "${GREEN}Fleet should have been deleted with success.${NC}\n"
+  log_info "Fleet should have been deleted with success."
 fi
