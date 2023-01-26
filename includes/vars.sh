e35a2d5ae2e8c5239eb1f907e24250f736bd70e6
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-01-26 19:31:28 +0000

    Update vars.sh

--- includes/vars.sh
+++ includes/vars.sh
@@ -1,8 +1,11 @@
-#Colors for logging
-export RED="\033[0;31m"
-export ORANGE="\033[0;33m"
-export GREEN="\033[0;32m"
-export NC="\033[0m"
-
-#Other vars
 export AXIOM_PATH="$HOME/.axiom"
+export MJOLNIR_OUT_FOLDER_PATH="$HOME/results"
+
+#WARNING! DO NOT CHANGE THESE UNLESS YOU KNOW WHAT YOU'RE DOING
+export MJOLNIR_OUT_SUBFOLDER_RECON="recon" #to store recon supermodule output
+export MJOLNIR_OUT_SUBFOLDER_SCAN="scan" #to store scan supermodule output
+export MJOLNIR_OUT_SUBFOLDER_EXPLOIT="exploit" #to store exploit supermodule output
+export RED="\033[0;31m" #logging color
+export ORANGE="\033[0;33m" #logging color
+export GREEN="\033[0;32m" #logging color
+export NC="\033[0m" #logging color
