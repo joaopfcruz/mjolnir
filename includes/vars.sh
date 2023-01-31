27f75d785086843d502634a2797bc99f7ac58cea
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-01-31 18:17:15 +0000

    Added Slack integration

--- includes/vars.sh
+++ includes/vars.sh
@@ -1,5 +1,12 @@
+#Paths
 export AXIOM_PATH="$HOME/.axiom"
 export MJOLNIR_OUT_FOLDER_PATH="$HOME/results"
+
+#tokens
+export SLACK_API_TOKEN="xoxb-......"
+
+#Slack config
+export SLACK_CHANNEL_ID_FULLACTIVITY="C04MB1N2XH8"
 
 #WARNING! DO NOT CHANGE THESE UNLESS YOU KNOW WHAT YOU'RE DOING
 export MJOLNIR_OUT_SUBFOLDER_RECON="recon" #to store recon supermodule output
@@ -9,3 +16,8 @@ export RED="\033[0;31m" #logging color
 export ORANGE="\033[0;33m" #logging color
 export GREEN="\033[0;32m" #logging color
 export NC="\033[0m" #logging color
+export SLACK_EMOJI_GREEN_CIRCLE=":large_green_circle:" #Slack cosmetics
+export SLACK_EMOJI_ORANGE_CIRCLE=":large_orange_circle:" #Slack cosmetics
+export SLACK_EMOJI_RED_CIRCLE=":red_circle:" #Slack cosmetics
+export SLACK_EMOJI_START_PROCESS=":hammer:" #Slack cosmetics
+export SLACK_EMOJI_FINISH_PROCESS=":checkered_flag:" #Slack cosmetics
