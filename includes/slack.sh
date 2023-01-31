15ff8d4079a1a2ed4b31b314872b756cfc7faf62
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-01-31 18:16:07 +0000

    Added Slack integration

---
+++ includes/slack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source "$MJOLNIR_PATH/includes/vars.sh"
+
+slack_notification() {
+  if [ "$1" = true ] ; then
+    log_info "Sending Slack notification to channel $3. API answer:"
+    curl_output=$(curl -s -d "text=$2" -d "channel=$3" -H "Authorization: Bearer ${SLACK_API_TOKEN}" -X POST https://slack.com/api/chat.postMessage)
+    printf "${curl_output}\n"
+  fi
+}
