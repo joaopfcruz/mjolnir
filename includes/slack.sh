aad925717a586f4d3502c77c198816f83ac80f29
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-02-08 20:43:25 +0000

    Update slack.sh

--- includes/slack.sh
+++ includes/slack.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 source "$HOME/.slackenv"
 
+SCRIPTNAME_SLACK="slack.sh"
+
 slack_notification() {
   if [ "$1" = true ] ; then
-    log_info "Sending Slack notification to channel $3. API answer:"
+    log_info "Sending Slack notification to channel $3. API answer:" "${SCRIPTNAME_SLACK}"
     curl_output=$(curl -s -d "text=$2" -d "channel=$3" -H "Authorization: Bearer ${SLACK_API_TOKEN}" -X POST https://slack.com/api/chat.postMessage)
     printf "${curl_output}\n"
   fi
