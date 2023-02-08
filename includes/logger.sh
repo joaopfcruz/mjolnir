d7866610406722fb8e19af694d9243a63afae518
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-02-08 20:42:26 +0000

    Update logger.sh

--- includes/logger.sh
+++ includes/logger.sh
@@ -3,13 +3,12 @@
 source "$MJOLNIR_PATH/includes/vars.sh"
 
 log_info(){
-  printf "${GREEN}[$(date '+%Y-%m-%d %H:%M:%S')] [mjolnir] [INFO] $1\n${NC}"
+  printf "${GREEN}[$(date '+%Y-%m-%d %H:%M:%S')] [mjolnir] [$2] [INFO] $1\n${NC}"
 }
 
 log_warn(){
-  printf "${ORANGE}[$(date '+%Y-%m-%d %H:%M:%S')] [mjolnir] [WARN] $1\n${NC}"
+  printf "${ORANGE}[$(date '+%Y-%m-%d %H:%M:%S')] [mjolnir] [$2] [WARN] $1\n${NC}"
 }
 
 log_err(){
-  printf "${RED}[$(date '+%Y-%m-%d %H:%M:%S')] [mjolnir] [ERROR] $1\n${NC}" >&2
-}
+  printf "${RED}[$(date '+%Y-%m-%d %H:%M:%S')] [mjolnir] [$2] [ERROR] $1\n${NC}" >&2
