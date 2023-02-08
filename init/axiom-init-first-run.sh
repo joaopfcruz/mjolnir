5322be926359d078839af275a7dc6bd6e3920d37
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-02-08 12:40:28 +0000

    Update axiom-init-first-run.sh

--- init/axiom-init-first-run.sh
+++ init/axiom-init-first-run.sh
@@ -101,14 +101,6 @@ doctl compute image update ${image_id} --image-name ${GOLDEN_IMAGE_NAME}
 account_path=$(ls -la $AXIOM_PATH/axiom.json | rev | cut -d " " -f 1 | rev)
 jq '.imageid="'${GOLDEN_IMAGE_NAME}'"' < "$account_path" > "$AXIOM_PATH"/tmp.json ; mv "$AXIOM_PATH"/tmp.json "$account_path"
 printf "${GREEN}\nDone. Image should be now named '${GOLDEN_IMAGE_NAME}'.\n${NC}"
-
-sleep $SLEEPTIME
-
-printf "${GREEN}\n\n\n*****************************\n"
-printf "Removing passwordless user as it's not needed anymore!\n"
-printf "*****************************\n\n${NC}"
-sudo sed -i "/${APPUSER}.*NOPASSWD:.*ALL/d" /etc/sudoers
-printf "${GREEN}\nDone.\n${NC}"
 sleep $SLEEPTIME
 
 printf "${GREEN}\n\n\n'op' user password is: $(jq -r '.op' $AXIOM_PATH/axiom.json)\n"
