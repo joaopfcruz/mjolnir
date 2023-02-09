d436e59a6167aea8f1d01177a5a12eb592b54edf
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-02-09 19:40:55 +0000

    Update vars.sh

--- includes/vars.sh
+++ includes/vars.sh
@@ -1,17 +1,18 @@
 #Paths
-export AXIOM_PATH="$HOME/.axiom"
-export MJOLNIR_OUT_FOLDER_PATH="$HOME/results"
+AXIOM_PATH="$HOME/.axiom"
+MJOLNIR_OUT_FOLDER_PATH="$HOME/results"
 
 #WARNING! DO NOT CHANGE THESE UNLESS YOU KNOW WHAT YOU'RE DOING
-export MJOLNIR_OUT_SUBFOLDER_RECON="recon" #to store recon supermodule output
-export MJOLNIR_OUT_SUBFOLDER_SCAN="scan" #to store scan supermodule output
-export MJOLNIR_OUT_SUBFOLDER_EXPLOIT="exploit" #to store exploit supermodule output
-export RED="\033[0;31m" #logging color
-export ORANGE="\033[0;33m" #logging color
-export GREEN="\033[0;32m" #logging color
-export NC="\033[0m" #logging color
-export SLACK_EMOJI_GREEN_CIRCLE=":large_green_circle:" #Slack cosmetics
-export SLACK_EMOJI_ORANGE_CIRCLE=":large_orange_circle:" #Slack cosmetics
-export SLACK_EMOJI_RED_CIRCLE=":red_circle:" #Slack cosmetics
-export SLACK_EMOJI_START_PROCESS=":hammer:" #Slack cosmetics
-export SLACK_EMOJI_FINISH_PROCESS=":checkered_flag:" #Slack cosmetics
+MAX_FLEET_NUMBER=25
+MJOLNIR_OUT_SUBFOLDER_RECON="recon" #to store recon supermodule output
+MJOLNIR_OUT_SUBFOLDER_SCAN="scan" #to store scan supermodule output
+MJOLNIR_OUT_SUBFOLDER_EXPLOIT="exploit" #to store exploit supermodule output
+RED="\033[0;31m" #logging color
+ORANGE="\033[0;33m" #logging color
+GREEN="\033[0;32m" #logging color
+NC="\033[0m" #logging color
+SLACK_EMOJI_GREEN_CIRCLE=":large_green_circle:" #Slack cosmetics
+SLACK_EMOJI_ORANGE_CIRCLE=":large_orange_circle:" #Slack cosmetics
+SLACK_EMOJI_RED_CIRCLE=":red_circle:" #Slack cosmetics
+SLACK_EMOJI_START_PROCESS=":hammer:" #Slack cosmetics
+SLACK_EMOJI_FINISH_PROCESS=":checkered_flag:" #Slack cosmetics
