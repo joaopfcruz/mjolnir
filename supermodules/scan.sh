15263115bfdd72aec66256964a0e1ff7695938cf
joaopfcruz <63175439+joaopfcruz@users.noreply.github.com>
2023-03-14 00:00:59 +0000

    Update scan.sh

--- supermodules/scan.sh
+++ supermodules/scan.sh
@@ -135,30 +135,30 @@ else
 fi
 
 
-${MJOLNIR_PATH}/fleetmgmt/axiomfleet-spawn.sh -p "${fleet}" -n ${fleet_count} "${notify_flag_fleet_cmd}"
+#${MJOLNIR_PATH}/fleetmgmt/axiomfleet-spawn.sh -p "${fleet}" -n ${fleet_count} "${notify_flag_fleet_cmd}"
 
 
 ##NAABU##
 naabu_output="/tmp/naabu.out"
-rm -f ${naabu_output}
+#rm -f ${naabu_output}
 log_info "Running naabu module (input file: ${scan_inputfile} ; output file: ${naabu_output}; fleet: ${fleet})..." "${SCRIPTNAME_SCAN}"
 slack_notification "${notify_flag}" "${SLACK_EMOJI_GREEN_CIRCLE} [_${SCRIPTNAME_SCAN}_] Running \`naabu\` module on fleet \`${fleet}\`" "${SLACK_CHANNEL_ID_FULLACTIVITY}"
-axiom-scan ${scan_inputfile} -m naabu -o ${naabu_output} --fleet "${fleet}*" -p - -c 100 -rate 10000 -sa -iv 4,6 -Pn -retries 3
+#axiom-scan ${scan_inputfile} -m naabu -o ${naabu_output} --fleet "${fleet}*" -p - -c 100 -rate 10000 -sa -iv 4,6 -Pn -retries 3
 
 ##NMAP##
 nmap_output="/tmp/nmap.out"
-rm -f ${nmap_scans_file} ${nmap_output}
+#rm -f ${nmap_scans_file} ${nmap_output}
 discovered_ports=$(cat ${naabu_output} | cut -d ":" -f 2 | sort -u)
 discovered_ports=$(echo -n "${discovered_ports}" | tr "\n" ",")
 #run nmap against all hosts and all discovered ports
 #(not all ports will be found in all hosts but that's fine (for the sake of simplicity))
 log_info "Running nmap module (input file: ${scan_inputfile} ; output file: ${nmap_output}; fleet: ${fleet})..." "${SCRIPTNAME_SCAN}"
 slack_notification "${notify_flag}" "${SLACK_EMOJI_GREEN_CIRCLE} [_${SCRIPTNAME_SCAN}_] Running \`nmap\` module on fleet \`${fleet}\`" "${SLACK_CHANNEL_ID_FULLACTIVITY}"
-axiom-scan ${scan_inputfile} -m nmap -oX ${nmap_output} --fleet "${fleet}*" -p ${discovered_ports} -Pn -sS -sC -T4 -sV --min-rate 1000 --max-retries 3 --open
-rm -f ${nmap_output}.html
+#axiom-scan ${scan_inputfile} -m nmap -oX ${nmap_output} --fleet "${fleet}*" -p ${discovered_ports} -Pn -sS -sC -T4 -sV --min-rate 1000 --max-retries 3 --open
+#rm -f ${nmap_output}.html
 
 httpx_output="/tmp/httpx.out"
-rm -f ${httpx_output}
+#rm -f ${httpx_output}
 #we're not interested in grouping hosts by open ports, but it's a quick and dirty way to get the correspondent hostname of an IP at this point,
 #since host-ports will give us ip_port data only
 group_by_ports_data=$($MJOLNIR_PATH/auxiliary/nmap-parse-output/nmap-parse-output ${nmap_output} group-by-ports)
@@ -169,13 +169,66 @@ https_ports=$(echo "${http_ports_data}" | grep -P "^https://" | cut -d ":" -f 3 
 #(not all ports will be found in all hosts but that's fine (for the sake of simplicity))
 log_info "Running httpx module (input file: ${scan_inputfile} ; output file: ${httpx_output}; fleet: ${fleet})..." "${SCRIPTNAME_SCAN}"
 slack_notification "${notify_flag}" "${SLACK_EMOJI_GREEN_CIRCLE} [_${SCRIPTNAME_SCAN}_] Running \`httpx\` module on fleet \`${fleet}\`" "${SLACK_CHANNEL_ID_FULLACTIVITY}"
-axiom-scan ${scan_inputfile} -m httpx --fleet "${fleet}*" -o ${httpx_output} -p http:${http_ports},https:${https_ports} -sc -cl -ct -location -hash sha512 -jarm -title -server -tech-detect -json -random-agent -fr -timeout 3
+#axiom-scan ${scan_inputfile} -m httpx --fleet "${fleet}*" -o ${httpx_output} -p http:${http_ports},https:${https_ports} -sc -cl -ct -location -hash sha512 -jarm -title -server -tech-detect -json -random-agent -fr -timeout 3
 
 #rm -f ${scan_inputfile}
 #rm -f ${naabu_output}
 #rm -f ${nmap_output}
 #rm -f ${httpx_output}
 
+#converting output to json
+python3 - << EOF
+import json
+from bisect import bisect_left
+from lxml import etree
+
+data={"scan":{"firstpassscan":{"description":"First pass scan takes all domains and subdomains as input and quickly scan all range of TCP ports from 1 to 65535","results":{}},"secondpassscan":{"description":"The second pass scan receives the results of first pass scan as input and perform a more targeted scan on those ports only","results":{}},"targeteddiscovery":{"description":"After first and second pass scans, some additional discovery scans are executed to find ports running specific services which can then be used to perform focused exploitation on those services","results":{}}}}
+
+#naabu output
+with open("${naabu_output}") as f:
+  for line in f:
+    domain, port = line.strip().split(':')
+    if domain in data["scan"]["firstpassscan"]["results"]:
+      #using bisect_left to insert the port in the correct index and keep the array sorted
+      pos = bisect_left(data["scan"]["firstpassscan"]["results"][domain], int(port))
+      data["scan"]["firstpassscan"]["results"][domain].insert(pos, int(port))
+    else:
+      data["scan"]["firstpassscan"]["results"][domain] = [int(port)]
+
+#nmap output
+x = 0
+nmap_data = {}
+nmap_xml_tree = etree.parse("${nmap_output}")
+hosts_elements = nmap_xml_tree.findall(".//host")
+for h_xmlelem in hosts_elements:
+  ports_elements = h_xmlelem.findall(".//ports")
+  for p_xmlelem in ports_elements:
+    port_elements = p_xmlelem.findall(".//port")
+    for sp_xmlelem in port_elements:
+      p_state_elements = sp_xmlelem.find(".//state")
+      if p_state_elements.attrib["state"] == "open":
+        addr_elements = h_xmlelem.find(".//address")
+        hostnames_elements = h_xmlelem.findall(".//hostnames/hostname")
+        protocol, port = sp_xmlelem.attrib["protocol"] if "protocol" in sp_xmlelem.attrib.keys() else "unknown", sp_xmlelem.attrib["portid"] if "portid" in sp_xmlelem.attrib.keys() else "unknown"
+        p_service_elements = p_xmlelem.find(".//service")
+        p_service_cpe_element = p_xmlelem.find(".//cpe")
+        cpe = p_service_cpe_element.text if p_service_cpe_element is not None else "unknown"
+        service_name, service_product, service_version = p_service_elements.attrib["name"] if "name" in p_service_elements.attrib.keys() else "unknown", p_service_elements.attrib["product"] if "product" in p_service_elements.attrib.keys() else "unknown", p_service_elements.attrib["version"] if "version" in p_service_elements.attrib.keys() else "unknown"
+        if hostnames_elements:
+          for hostname_xmlelem in hostnames_elements:
+            h_type = hostname_xmlelem.attrib["type"] if "type" in hostname_xmlelem.attrib.keys() else "unknown"
+            if h_type == "user":
+              name = hostname_xmlelem.attrib["name"] if "name" in hostname_xmlelem.attrib.keys() else "unknown"
+              nmap_data[f"{name}:{port}"] = {"protocol": protocol, "service_name": service_name, "service_product": service_product, "service_version": service_version, "cpe": cpe}
+        else:
+              addr = addr_elements.attrib["addr"] if "addr" in addr_elements.attrib.keys() else "unknown"
+              nmap_data[f"{addr}:{port}"] = {"protocol": protocol, "service_name": service_name, "service_product": service_product, "service_version": service_version, "cpe": cpe}
+
+nmap_data_sorted = {k: nmap_data[k] for k in sorted(nmap_data)}
+data["scan"]["secondpassscan"]["results"] = nmap_data_sorted
+
+EOF
+exit
 #compress output
 log_info "Compressing output" "${SCRIPTNAME_SCAN}"
 bzip2 -z ${jsonoutput}
